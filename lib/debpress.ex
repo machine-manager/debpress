c8f5a83b9f73e4d62c49ccfe3ff17ceba1d1358b
Ivan Kozik <ivan@ludios.org>
2016-09-06 15:42:31 +0000

    Work on make_control

--- lib/debpress.ex
+++ lib/debpress.ex
@@ -2,34 +2,65 @@ defmodule Debpress do
 	@type Priority.t :: :required | :important | :standard | :optional | :extra
 
 	defmodule Control do
+		@enforce_keys [:name, :version, :architecture, :depends, :short_description]
 		defstruct
 			name: nil,
 			version: nil,
 			architecture: nil,
 			maintainer: "Nobody <nobody@localhost>",
-			installed_size_kb: nil,
+			installed_size_kb: 0,
 			depends: nil,
+			# See docs/sections.html
 			section: nil,
-			priority: :optional,
+			priority: nil,
 			short_description: nil
 			long_description: ""
 		@type t :: %User{name: String.t, age: non_neg_integer}
 	end
 
+	@spec prefix_every_line()
+	defp prefix_every_line(text, prefix) do
+		text |> String.split("\n") |> Enum.map(fn line -> prefix <> line end)
+	end
+
 	@spec make_control(Control) :: String.t
-	def make_control() do
+	def make_control(c) do
 		"""
 		Package: #{name}
 		Version: #{version}
 		Architecture: #{architecture}
 		Maintainer: #{maintainer}
-		Installed-Size: #{installed_size_kb}
-		Depends: #{depends}
-		Section: #{section}
-		Priority: #{priority |> Atom.to_string}
-		Description: #{short_description}
-		 #{long_description}
 		"""
+
+		if installed_size_kb, do
+			s = s <> "Installed-Size: #{c.installed_size_kb}\n"
+		end
+
+		if pre_depends do
+			s = s <> "Pre-Depends: #{c.pre_depends}\n"
+		end
+
+		if depends do
+			s = s <> "Depends: #{c.depends}\n"
+		end
+
+		if provides do
+			s = s <> "Provides: #{c.provides}\n"
+		end
+
+		if priority do
+			s = s <> "Priority: #{c.priority |> Atom.to_string}\n"
+		end
+
+		if section do
+			s = s <> "Section: #{c.section}\n"
+		end
+
+		s = s <> "Description: #{c.short_description}\n"
+
+		if long_description do
+			s = s <> prefix_every_line(long_description, " ") <> "\n"
+		end
 	end
 
 	@spec write_deb(String.t) :: none
