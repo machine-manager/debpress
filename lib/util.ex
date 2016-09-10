9b7283cf39aa229f02b24d3a5d6f72620b666246
Ivan Kozik <ivan@ludios.org>
2016-09-10 01:12:10 +0000

    Turn append_if into a macro to avoid blowing up on nils

--- lib/util.ex
+++ lib/util.ex
@@ -12,11 +12,24 @@ defmodule Debpress.Util do
 		end
 	end
 
-	def append_if(acc, condition, string) do
-		if condition do
-			acc <> string
-		else
-			acc
+	@doc ~S"""
+	For use in a pipeline like so:
+
+		s
+		|> append_if(c.section, "Section: #{c.section}\n")
+		|> append_if(true, "Description: #{c.short_description}\n")
+		|> append_if(c.long_description, prefix_every_line(c.long_description, " ") <> "\n")
+
+	expression is not evaluated unless evaluation of `clause` is truthy.  This avoids
+	blowing up on nils and other unexpected values.
+	"""
+	defmacro append_if(acc, clause, expression) do
+		quote do
+			if unquote(clause) do
+				unquote(acc) <> unquote(expression)
+			else
+				unquote(acc)
+			end
 		end
 	end
 end
