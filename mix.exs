f8aaa0a69109e135bbfa6d9c9edc958c3bb7ea0b
Ivan Kozik <ivan@ludios.org>
2016-10-03 15:24:32 +0000

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Debpress.Mixfile do
 	def project do
 		[
 			app: :debpress,
-			version: "0.2.0",
+			version: "0.2.1",
 			elixir: "~> 1.4-dev",
 			build_embedded: Mix.env == :prod,
 			start_permanent: Mix.env == :prod,
